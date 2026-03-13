b76ae64fdd4bdb728996e867af19a5e0b79d82cf
4ak45h <apjatikart@gmail.com>
2026-03-13 23:04:20 +0530

    lane

--- devworld/src/CountryView.jsx
+++ devworld/src/CountryView.jsx
@@ -1,35 +1,35 @@
-import State from "./State"
+// import State from "./State"
 
-export default function CountryView({ enterGithub }) {
+// export default function CountryView({ enterGithub }) {
 
-  return (
-    <group>
+//   return (
+//     <group>
 
-      {/* State 1 */}
+//       {/* State 1 */}
 
-      <State
-        name="Power"
-        color="#2f2f2f"
-        position={[0,0.3,0]}
-        onClick={enterGithub}
-      />
+//       <State
+//         name="Power"
+//         color="#2f2f2f"
+//         position={[0,0.3,0]}
+//         onClick={enterGithub}
+//       />
 
-      {/* State 2 */}
+//       {/* State 2 */}
 
-      <State
-        name="Social"
-        color="#1f3a5f"
-        position={[18,0.3,-10]}
-      />
+//       <State
+//         name="Social"
+//         color="#1f3a5f"
+//         position={[18,0.3,-10]}
+//       />
 
-      {/* State 3 */}
+//       {/* State 3 */}
 
-      <State
-        name="Unknown"
-        color="#3a2f2f"
-        position={[-18,0.3,-10]}
-      />
+//       <State
+//         name="Unknown"
+//         color="#3a2f2f"
+//         position={[-18,0.3,-10]}
+//       />
 
-    </group>
-  )
-}
+//     </group>
+//   )
+// }
