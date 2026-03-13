5ee71262ee052c705c71be4cadebde0baadaa31c
4ak45h <apjatikart@gmail.com>
2026-03-14 00:01:20 +0530

    lane

--- devworld/src/GrassField.jsx
+++ devworld/src/GrassField.jsx
@@ -1,37 +1,37 @@
-// import { useMemo } from "react"
+import { useMemo } from "react"
 
-// export default function GrassField() {
+export default function GrassField() {
 
-//   const patches = useMemo(()=>{
+  const patches = useMemo(()=>{
 
-//     const items = []
-//     const size = 200
-//     const count = 400
+    const items = []
+    const size = 200
+    const count = 400
 
-//     for(let i=0;i<count;i++){
+    for(let i=0;i<count;i++){
 
-//       const x = (Math.random()-0.5)*size
-//       const z = (Math.random()-0.5)*size
-//       const scale = 0.6 + Math.random()*1.4
+      const x = (Math.random()-0.5)*size
+      const z = (Math.random()-0.5)*size
+      const scale = 0.6 + Math.random()*1.4
 
-//       items.push(
+      items.push(
 
-//         <mesh
-//           key={i}
-//           position={[x,0.05,z]}
-//           rotation={[0,Math.random()*Math.PI,0]}
-//           scale={scale}
-//         >
-//           <coneGeometry args={[0.5,1.2,6]} />
-//           <meshStandardMaterial color="#1b5e20"/>
-//         </mesh>
+        <mesh
+          key={i}
+          position={[x,0.05,z]}
+          rotation={[0,Math.random()*Math.PI,0]}
+          scale={scale}
+        >
+          <coneGeometry args={[0.5,1.2,6]} />
+          <meshStandardMaterial color="#1b5e20"/>
+        </mesh>
 
-//       )
-//     }
+      )
+    }
 
-//     return items
+    return items
 
-//   },[])
+  },[])
 
-//   return <group>{patches}</group>
-// }
+  return <group>{patches}</group>
+}
