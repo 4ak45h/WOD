ae38ff29a9f63ffdd23273ab61a37aa91dc28455
4ak45h <apjatikart@gmail.com>
2026-03-15 23:13:21 +0530

    island

--- devworld/src/ForgeView.jsx
+++ devworld/src/ForgeView.jsx
@@ -1,32 +1,46 @@
 import StateTile from "./StateTile"
-import ForgeIsland from "./ForgeIsland"
-import CityBorder from "./CityBorder"
 
-export default function ForgeView({ enterGithub }){
+export default function ForgeView({ enterGithub }) {
 
-  return(
+  return (
 
     <group>
 
-      {/* island terrain */}
-      <ForgeIsland/>
+      {/* Forge land */}
 
-      {/* city borders */}
+      <mesh rotation={[-Math.PI/2,0,0]} position={[0,0,0]}>
+        <planeGeometry args={[60,60]} />
+        <meshStandardMaterial color="#555555" />
+      </mesh>
 
-      <CityBorder position={[0,0.2,0]} width={40} depth={0.4}/>
-      <CityBorder position={[0,0.2,-8]} width={40} depth={0.4}/>
-
-      <CityBorder position={[-10,0.2,-4]} width={0.4} depth={24}/>
-      <CityBorder position={[10,0.2,-4]} width={0.4} depth={24}/>
 
       {/* cities */}
 
-      <StateTile position={[0,0.5,4]} label="Github" onClick={enterGithub}/>
-      <StateTile position={[14,0.5,4]} label="Leetcode"/>
-      <StateTile position={[-14,0.5,4]} label="Hackerrank"/>
+      <StateTile
+        position={[0,0.5,4]}
+        label="Github"
+        onClick={enterGithub}
+      />
 
-      <StateTile position={[8,0.5,-10]} label="Sealed"/>
-      <StateTile position={[-8,0.5,-10]} label="Sealed"/>
+      <StateTile
+        position={[14,0.5,4]}
+        label="Leetcode"
+      />
+
+      <StateTile
+        position={[-14,0.5,4]}
+        label="Hackerrank"
+      />
+
+      <StateTile
+        position={[8,0.5,-10]}
+        label="Sealed"
+      />
+
+      <StateTile
+        position={[-8,0.5,-10]}
+        label="Sealed"
+      />
 
     </group>
 
